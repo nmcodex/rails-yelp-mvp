16e8ce72bdd74f541157efc821c00352648b66f1
NM <nm.spamming@gmail.com>
2018-02-14 12:30:42 +0100

    add validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
   has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, inclusion: { in: CATEGORY }
 end
